2b4f90d715249742aa62627d3abfdc463ac69144
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-08-10 23:19:01 +0100

    Initial Commit

---
+++ MyNotesVault/MyNotesVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesVaultApp.swift
+//  MyNotesVault
+//
+//  Created by BSTAR on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
